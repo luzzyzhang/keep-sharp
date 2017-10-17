5de108c3d67cdd341da7217785cb6316c50c2d55
luzzy <luzzyzhang@gmail.com>
2017-10-17 12:37:03 +0800

    Longest substring slide window

--- problems/p02_longest_substring_without_repeat_chars.py
+++ problems/p02_longest_substring_without_repeat_chars.py
@@ -11,6 +11,19 @@ def len_of_longest_substring_brute_force(s):
     return max_len
 
 
+def len_of_longest_substring_slide_window(s):
+    max_len = 0
+    left = 0
+    visited = {}
+    for right, char in enumerate(s):
+        if char in visited and visited[char] >= left:
+            left = visited[char] + 1
+        max_len = max(max_len, right-left+1)
+        visited[char] = right
+    return max_len
+
+
 if __name__ == '__main__':
     test, expect = 'abcabcbb', 3  # 'abc'
     assert len_of_longest_substring_brute_force(test) == expect
+    assert len_of_longest_substring_slide_window(test) == expect
--- problems/tests/test_2_longest_substring_without_repeat_chars.py
+++ problems/tests/test_2_longest_substring_without_repeat_chars.py
@@ -3,6 +3,7 @@ import pytest
 
 from problems.p02_longest_substring_without_repeat_chars import (
         len_of_longest_substring_brute_force,
+        len_of_longest_substring_slide_window,
 )
 
 
@@ -13,6 +14,7 @@ test_params = [
     ('abcabcbb', 3),
     ('bbbbb', 1),
     ('pwwkew', 3),
+    ('abcdef', 6),
     (long_string, 11)
 ]
 
@@ -20,3 +22,4 @@ test_params = [
 @pytest.mark.parametrize('string, expected', test_params)
 def test_longest_substring_no_repeat_chars(string, expected):
     assert len_of_longest_substring_brute_force(string) == expected
+    assert len_of_longest_substring_slide_window(string) == expected
