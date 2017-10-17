bd6f161ace4aaad9f879b0c3e82e28d073dea9c9
luzzy <luzzyzhang@gmail.com>
2017-10-17 11:35:05 +0800

    Polish broute fource version

--- problems/p02_longest_substring_without_repeat_chars.py
+++ problems/p02_longest_substring_without_repeat_chars.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 
 
-def len_of_longest_substring(s):
-
-    def all_unique(string):
-        return True if len(string) == len(set(string)) else False
-
+def len_of_longest_substring_brute_force(s):
     max_len = 0
     n = len(s)
     for i in range(n):
         for j in range(i+1, n+1):
-            if all_unique(s[i:j]):
+            if len(s[i:j]) == len(set(s[i:j])):  # or len(set(s[i:j]) == j-i
                 max_len = max(max_len, j-i)
     return max_len
 
@@ -19,9 +15,9 @@ if __name__ == '__main__':
     test1, expect1 = 'abcabcbb', 3  # 'abc'
     test2, expect2 = 'bbbbb', 1  # 'b'
     test3, expect3 = 'pwwkew', 3  # 'wke'
-    assert len_of_longest_substring(test1) == expect1
-    assert len_of_longest_substring(test2) == expect2
-    assert len_of_longest_substring(test3) == expect3
+    assert len_of_longest_substring_brute_force(test1) == expect1
+    assert len_of_longest_substring_brute_force(test2) == expect2
+    assert len_of_longest_substring_brute_force(test3) == expect3
     long_string = ("lursenhsaqzomihhopmfffywxjxnbsgonzitmq"
                    "loilduvkblansfvqdubahcupshobccrqrzd")
-    print(len_of_longest_substring(long_string))
+    print(len_of_longest_substring_brute_force(long_string))
