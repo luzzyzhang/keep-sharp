6bd28ce2d635ec29a9a6243c4a3e8caad5fe2622
luzzy <luzzyzhang@gmail.com>
2017-10-26 10:50:06 +0800

    Optimize power func

--- problems/p32_power.py
+++ problems/p32_power.py
@@ -23,6 +23,34 @@ def power(base, exponent):
     return 1 / result if exponent < 0 else result
 
 
+# 使用公式，递归法实现
+# a^n = a^(n/2) * a^(n/2), n 为偶数；a^n = a^(n-1)/2 * a^(n-1)/2 * a，n 为奇数
+# 右移运算替代除以 2，位与运算替代求余（%）运算判断奇偶;提高运算效率
+def power_formula_recursive(base, exponent):
+    if base == 0:
+        return 0
+    if exponent == 0:
+        return 1
+    abs_exponent = abs(exponent)
+
+    def power_abs_exponent(base, n):
+        if n == 0:
+            return 1
+        if n == 1:
+            return base
+        r = power_abs_exponent(base, n >> 1)
+        r *= r
+        if n & 0x1 == 1:
+            r *= base
+        return r
+
+    result = power_abs_exponent(base, abs_exponent)
+
+    return 1 / result if exponent < 0 else result
+
+
 if __name__ == '__main__':
     print(power(2, 3))
     print(power(2, -3))
+    print(power_formula_recursive(2, 3))
+    print(power_formula_recursive(2, -3))
