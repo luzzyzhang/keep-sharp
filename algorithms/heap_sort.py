3e02adcb3eedaef8d589cbfad7fc8c420dea8a97
luzzyzhang <luzzyzhang@users.noreply.github.com>
2017-11-22 18:26:49 +0800

    Update heap_sort.py

--- algorithms/heap_sort.py
+++ algorithms/heap_sort.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-“”“可以使用内置 heapq 实现
+"""可以使用内置 heapq 实现
    https://docs.python.org/3/library/heapq.html#basic-examples
 def heapsort(iterable):
 ...     h = []
@@ -9,7 +9,7 @@ def heapsort(iterable):
 ...
 >>> heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
 [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-”“”
+"""
 
 def heap_sort(lst):
     build_heap(lst)
