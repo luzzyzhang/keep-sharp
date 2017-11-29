2c64748a02c278e09c65504e39d5d552977ca103
luzzy <luzzyzhang@gmail.com>
2017-11-30 00:30:55 +0800

    Fix

--- problems/p54_get_k_least_numbers.py
+++ problems/p54_get_k_least_numbers.py
@@ -44,7 +44,7 @@ def get_least_use_heap2(numbers, k):
         else:
             largest = -h[0]
             if num < largest:
-                # h中最大被弹出，并且新元素推入
+                # h中最大元素 `largest` 被弹出并且新元素 `-num` 推入
                 heapq.heapreplace(h, -num)
     return [-heapq.heappop(h) for i in range(len(h))][::-1]
 
