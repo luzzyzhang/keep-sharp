0abc56daa632263760430bf449115cad8c29c9e3
luzzy <luzzyzhang@gmail.com>
2017-11-30 00:11:03 +0800

    Find k smallest numbers use heaq

--- problems/p54_get_k_least_numbers.py
+++ problems/p54_get_k_least_numbers.py
@@ -6,7 +6,7 @@
     这8个数字，则最小的4个数字是1、2、3、4。
 """
 
-# import heapq
+import heapq
 
 from util import partition
 
@@ -27,7 +27,29 @@ def get_least(numbers, k):
     return numbers[:k]
 
 
+def get_least_use_heap(numbers, k):
+    h = []
+    for num in numbers:
+        heapq.heappush(h, num)
+    return [heapq.heappop(h) for i in range(k)]
+
+
+def get_least_use_heap2(numbers, k):
+    h = []
+    for num in numbers:
+        if len(h) < k:
+            heapq.heappush(h, num)
+        else:
+            greatest = h[-1]
+            if num < greatest:
+                print(h.pop())
+                heapq.heappush(h, num)
+    return [heapq.heappop(h) for _ in range(len(h))]
+
+
 if __name__ == '__main__':
     numbers = [4, 5, 1, 6, 2, 7, 3, 8]
     expect = [1, 2, 3, 4]
-    assert get_least(numbers, 4) == expect
+    # assert get_least(numbers, 4) == expect
+    assert get_least_use_heap(numbers, 4) == expect
+    print(get_least_use_heap2(numbers, 4))
