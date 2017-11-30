3abcc0d04952d0098ce77ae2469c3f827e115bae
zhanglu <zhanglu@breadtrip.com>
2017-11-30 13:55:41 +0800

    Find meidian from data stream.
    * Approach 2 use two heap
    * Use max_heap and min_heap find meidian
    
    https://leetcode.com/problems/find-median-from-data-stream/solution/

--- problems/p55_find_median_from_data_stream.py
+++ problems/p55_find_median_from_data_stream.py
@@ -5,10 +5,12 @@
     https://leetcode.com/problems/find-median-from-data-stream/description/
 """
 
+from heapq import heappush, heappop
+
 
 class MedianFinder:
     """Approach 1:
-    Store the numbers in a resize-able container. 
+    Store the numbers in a resize-able container.
     Every time you need to output the median,
     sort the container and output the median.
 
@@ -37,8 +39,31 @@ class MedianFinder:
         return self.store[n//2] if n & 1 else (self.store[n//2 - 1] + self.store[n//2])*0.5
 
 
+class MedianFinder2:
+
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []
+
+    def add_num(self, num):
+        heappush(self.max_heap, -num)
+        heappush(self.min_heap, abs(self.max_heap[0]))
+        heappop(self.max_heap)
+
+        if len(self.max_heap) < len(self.min_heap):
+            heappush(self.max_heap, -self.min_heap[0])
+            heappop(self.min_heap)
+
+    def find_median(self):
+        if len(self.max_heap) > len(self.min_heap):
+            return abs(self.max_heap[0])
+        else:
+            return (abs(self.min_heap[0]) + abs(self.max_heap[0]))*0.5
+
+
 if __name__ == '__main__':
-    obj = MedianFinder()
+    # obj = MedianFinder()
+    obj = MedianFinder2()
     obj.add_num(1)
     print(obj.find_median())
     obj.add_num(2)
@@ -46,4 +71,6 @@ if __name__ == '__main__':
     obj.add_num(3)
     print(obj.find_median())
     obj.add_num(4)
+    print(obj.find_median())
+    obj.add_num(5)
     print(obj.find_median())
