47f1fe9b29a4ed386a24a3b404c2ede2a7a33a05
zhanglu <zhanglu@breadtrip.com>
2017-12-06 10:43:05 +0800

    Add new problem

--- problems/p67_two_sum_in_sorted_array.py
+++ problems/p67_two_sum_in_sorted_array.py
@@ -17,6 +17,26 @@ def two_sum_in_sorted_array(data, target):
     return -1
 
 
+def continue_sequence_of_sum(target):
+    if target < 3:
+        return
+    small, big = 1, 2
+    middle = (1+target) // 2
+    current_sum = small + big
+
+    while small < big:
+        if current_sum == target:
+            yield (small, big)
+        while current_sum > target and small < middle:
+            current_sum -= small
+            small += 1
+
+            if current_sum == target:
+                yield (small, big)
+        big += 1
+        current_sum += big
+
+
 if __name__ == '__main__':
     # TODO test
     pass
